5d88d5ec1226e276391a21827cf103865b4883ad
Picbel <picbel.gon@gmail.com>
2022-03-08 16:15:54 +0900

    Initial commit

---
+++ src/main/java/com/picbel/collectmap/CollectmapApplication.java
@@ -0,0 +1,13 @@
+package com.picbel.collectmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectmapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollectmapApplication.class, args);
+    }
+
+}
